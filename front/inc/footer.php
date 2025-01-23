87c2c0dd545685b3ec9d31d0982a6338ba689222
miok <aluvy_@naver.com>
2025-01-23 22:58:18 +0900

    수정

--- front/inc/footer.php
+++ front/inc/footer.php
@@ -1,29 +1,33 @@
-<div class="scrolldown">
-      <button type="button" id="scrolldown_button"></button>
-    </div>
+    <?php if( $basename == 'index.php') { ?>
+      <div class="scrolldown">
+        <button type="button" id="scrolldown_button"></button>
+      </div>
+    <?php } ?>
 
   </div><!-- #wrap -->
 
 
-  <div id="popup" class="use">
-    <div class="popup_wrap">
-      <div class="popup_body">
-        <div class="image">
-          <div class="img_pc"><img src="assets/img/popup_pc.png" alt=""></div>
-          <div class="img_mo"><img src="assets/img/popup_mo.png" alt=""></div>
+  <?php if( $basename == 'index.php') { ?>
+    <div id="popup" class="use">
+      <div class="popup_wrap">
+        <div class="popup_body">
+          <div class="image">
+            <div class="img_pc"><img src="assets/img/popup_pc.png" alt=""></div>
+            <div class="img_mo"><img src="assets/img/popup_mo.png" alt=""></div>
+          </div>
+          <div class="desc">
+            2025년, 특허법인 한남이 ‘<span>N E X T</span>’로 새롭게 시작합니다.
+          </div>
         </div>
-        <div class="desc">
-          2025년, 특허법인 한남이 ‘<span>N E X T</span>’로 새롭게 시작합니다.
+        <button type="button" class="btn_close" id="popup_close"></button>
+        <div class="today">
+          <input type="checkbox" id="today" class="blind">
+          <label for="today">오늘 하루 보지 않기</label>
         </div>
       </div>
-      <button type="button" class="btn_close" id="popup_close"></button>
-      <div class="today">
-        <input type="checkbox" id="today" class="blind">
-        <label for="today">오늘 하루 보지 않기</label>
-      </div>
+      <div class="dimmed"></div>
     </div>
-    <div class="dimmed"></div>
-  </div>
+  <?php } ?>
 
 </body>
 </html>
